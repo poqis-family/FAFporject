3b4f0e49d30d20302ce2dc547705526b57a84f7f
CxHanCxHan <305928360@qq.com>
2019-08-25 22:44:48 +0800

    策划案完善

---
+++ FAFproject/Assets/Scripts/SenceJump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SenceJump : MonoBehaviour
+{
+    AsyncOperation test;
+    bool IsLoading;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Click()
+    {
+        AsyncOperation test= SceneManager.LoadSceneAsync("Restrant");
+        bool IsLoading = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+     if(IsLoading)   Debug.Log(test.progress);
+    }
+}

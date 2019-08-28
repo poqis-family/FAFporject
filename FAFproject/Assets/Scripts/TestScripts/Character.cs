b0c8a5690bc36e070b09369822b9cd069514005f
CxHanCxHan <305928360@qq.com>
2019-08-28 17:49:58 +0800

    testScript

---
+++ FAFproject/Assets/Scripts/TestScripts/Character.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Character
+{
+
+    public string name;
+
+    public Character()
+    {
+        this.name = "";
+    }
+}
---
+++ FAFproject/Assets/Scripts/TestScripts/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Game
+{
+
+    public static Game current;
+    public Character knight;
+    public Character rogue;
+    public Character wizard;
+
+    public Game()
+    {
+        knight = new Character();
+        rogue = new Character();
+        wizard = new Character();
+    }
+
+}

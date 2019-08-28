f4102b0a336eeb854ddfe8250bc5d79a6d4b4e01
CxHanCxHan <305928360@qq.com>
2019-08-28 18:43:26 +0800

    update

---
+++ FAFproject/Assets/Scripts/Data/MainData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public static class MainData 
+{
+    public static int year;
+    public static int month;
+    public static string name;
+    public static int age;
+
+}
--- FAFproject/Assets/Scripts/MainScreen/MainInfo.cs
+++ FAFproject/Assets/Scripts/MainScreen/MainInfo.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainInfo : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        MainData.year = 191;
+        MainData.month = 4;
+        MainData.name = "Pochi";
+        MainData.age = 8;
+        transform.Find("Year").GetComponent<Text>().text = MainData.year.ToString();
     }
 
     // Update is called once per frame

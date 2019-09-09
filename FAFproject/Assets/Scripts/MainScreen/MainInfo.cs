907186cbd240990a9ec4c6078a3361c446d95c43
CxHanCxHan <305928360@qq.com>
2019-09-09 21:48:27 +0800

    读表（除了excel中 有些数据会读取int，和多张表的情况未测试

--- FAFproject/Assets/Scripts/MainScreen/MainInfo.cs
+++ FAFproject/Assets/Scripts/MainScreen/MainInfo.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using WJExcelDataManager;
+
+using WJExcelDataClass;
 
 public class MainInfo : MonoBehaviour
 {
@@ -13,6 +16,10 @@ public class MainInfo : MonoBehaviour
         MainData.name = "111";
         MainData.age = 111;
         MainData.LoadData();
+        DataManager dataManager = new DataManager();
+        dataManager.LoadAll();
+        Sheet1Item sheet1Item= dataManager.GetSheet1ItemByID("AAA1");
+        MainData.name =sheet1Item.name;
         transform.Find("year").GetComponent<Text>().text = MainData.year.ToString();
         transform.Find("month").GetComponent<Text>().text = MainData.month.ToString();
         transform.Find("name").GetComponent<Text>().text = MainData.name.ToString();

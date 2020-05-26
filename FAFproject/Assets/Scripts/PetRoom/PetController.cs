1e9838fb0aedba66264df8f224d45c3918474b8a
CxHanCxHan <42167951+CxHanCxHan@users.noreply.github.com>
2020-05-26 21:51:49 +0800

    更新寻路等测试

---
+++ FAFproject/Assets/Scripts/PetRoom/PetController.cs
@@ -0,0 +1,27 @@
+﻿using Pathfinding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetController : MonoBehaviour
+{
+    public Animator animator;
+    public AIPath aiPath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        aiPath = GetComponent<AIPath>();
+        Debug.Log("211111111");
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool("IsOnTrigger", true);
+        aiPath.canMove = false;
+        aiPath.Teleport(other.GetComponent<Transform>().position);
+        Debug.Log("2222222");
+    }
+}
---
+++ FAFproject/Assets/Scripts/player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Vector2 direction;
+    private Rigidbody2D rb;
+    public float speed;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2f;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        AnimatorMovement(direction);
+    }
+    public void AnimatorMovement(Vector2 derection)
+    {
+        animator.SetFloat("x", direction.x);
+        animator.SetFloat("y", direction.y);
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = direction.normalized * speed; ;
+    }
+    private void GetInput() {
+        direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+    }
+}

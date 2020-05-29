210381fdfd6bc24132c1dd0c93a5a1e81f2870df
CxHanCxHan <42167951+CxHanCxHan@users.noreply.github.com>
2020-05-29 11:05:56 +0800

    完成自动寻路

--- FAFproject/Assets/Scripts/AstarSupport.cs
+++ FAFproject/Assets/Scripts/AstarSupport.cs
@@ -1,91 +1,36 @@
 ﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
 // Note this line, if it is left out, the script won't know that the class 'Path' exists and it will throw compiler errors
 // This line should always be present at the top of scripts which use pathfinding
-using Pathfinding;
 public class AstarSupport : MonoBehaviour
 {
-    public Transform targetPosition;
-    private Seeker seeker;
-    private CharacterController controller;
-    public Path path;
-    public float speed = 2;
-    public float nextWaypointDistance = 3;
-    private int currentWaypoint = 0;
-    public bool reachedEndOfPath;
+    public float radius = 1000;
+    IAstarAI ai;
+    GraphNode randomNode;
     public void Start()
     {
-        
-        seeker = GetComponent<Seeker>();
-        // If you are writing a 2D game you can remove this line
-        // and use the alternative way to move sugggested further below.
-        controller = GetComponent<CharacterController>();
-        // Start a new path to the targetPosition, call the the OnPathComplete function
-        // when the path has been calculated (which may take a few frames depending on the complexity)
-        seeker.StartPath(transform.position, targetPosition.position, OnPathComplete);
+        ai = GetComponent<IAstarAI>();
+        var grid = AstarPath.active.data.gridGraph;
+        randomNode = grid.nodes[Random.Range(0, grid.nodes.Length)];
     }
-    public void OnPathComplete(Path p)
+    Vector3 PickRandomPoint()
     {
-        Debug.Log("A path was calculated. Did it fail with an error? " + p.error);
-        if (!p.error)
-        {
-            path = p;
-            // Reset the waypoint counter so that we start to move towards the first point in the path
-            currentWaypoint = 0;
-        }
+        var grid = AstarPath.active.data.gridGraph;
+        randomNode = grid.nodes[Random.Range(0, grid.nodes.Length)];
+        var destination1 = (Vector3)randomNode.position;
+        return destination1;
     }
     public void Update()
     {
+                // Update the destination of the AI if
+        // the AI is not already calculating a path and
+        // the ai has reached the end of the path or it has no path at all
+        if (!ai.pathPending && (ai.reachedEndOfPath || !ai.hasPath))
+        {
+            ai.destination = PickRandomPoint();
+            ai.SearchPath();
+        }
         AstarPath.active.Scan();
-        //AstarPathEditor.MenuScan();
-        if (path == null)
-        {
-            // We have no path to follow yet, so don't do anything
-            return;
-        }
-        // Check in a loop if we are close enough to the current waypoint to switch to the next one.
-        // We do this in a loop because many waypoints might be close to each other and we may reach
-        // several of them in the same frame.
-        reachedEndOfPath = false;
-        // The distance to the next waypoint in the path
-        float distanceToWaypoint;
-        
-        while (true)
-        {
-            // If you want maximum performance you can check the squared distance instead to get rid of a
-            // square root calculation. But that is outside the scope of this tutorial.
-            distanceToWaypoint = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
-            if (distanceToWaypoint < nextWaypointDistance)
-            {
-                // Check if there is another waypoint or if we have reached the end of the path
-                if (currentWaypoint + 1 < path.vectorPath.Count)
-                {
-                    currentWaypoint++;
-                }
-                else
-                {
-                    // Set a status variable to indicate that the agent has reached the end of the path.
-                    // You can use this to trigger some special code if your game requires that.
-                    reachedEndOfPath = true;
-                    break;
-                }
-            }
-            else
-            {
-                break;
-            }
-        }
-        // Slow down smoothly upon approaching the end of the path
-        // This value will smoothly go from 1 to 0 as the agent approaches the last waypoint in the path.
-        var speedFactor = reachedEndOfPath ? Mathf.Sqrt(distanceToWaypoint / nextWaypointDistance) : 1f;
-        // Direction to the next waypoint
-        // Normalize it so that it has a length of 1 world unit
-        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
-        // Multiply the direction by our desired speed to get a velocity
-        Vector3 velocity = dir * speed * speedFactor;
-        // Move the agent using the CharacterController component
-        // Note that SimpleMove takes a velocity in meters/second, so we should not multiply by Time.deltaTime
-        controller.SimpleMove(velocity);
-        // If you are writing a 2D game you may want to remove the CharacterController and instead use e.g transform.Translate
-        // transform.position += velocity * Time.deltaTime;
     }
 }

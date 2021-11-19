64c1b5c1c19f6cf88f7a6f921d949d60df4611b5
CxHanCxHan <305928360@qq.com>
2021-11-19 11:04:14 +0800

    脚本+场景

---
+++ FAFproject/Assets/Scripts/Farm/Backpack.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backpack : MonoBehaviour
+{
+    private int[] itemInfo = new int[2];
+    private List<int[]> ItemList = new List<int[]>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+        
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

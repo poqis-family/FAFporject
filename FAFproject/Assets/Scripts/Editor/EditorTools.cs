0e0fe490807fdd5103d757e54d855246d1b3c05d
305928360@qq.com <305928360@qq.com>
2021-12-14 14:52:50 +0800

    一个F5快速跳转到开始界面的功能

---
+++ FAFproject/Assets/Scripts/Editor/EditorTools.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using System.IO;
+
+public class EditorTools
+{
+    
+    [MenuItem("Edit/Run _F5")]
+    public static void OpenLaunchScene()
+    {
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/FarmStart.unity");
+        }
+    }
+    
+}
